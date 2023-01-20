54037a3339b20c7637c94a9d4b2ed0eb8f7e9d55
Anand A. Joshi <ajoshi@usc.edu>
2023-01-19 22:04:37 -0800

    update

--- dev/main_img2nii.py
+++ dev/main_img2nii.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import nibabel as nb
 import numpy as np
 from nilearn.image.resampling import reorder_img
-
+from nilearn.masking import apply_mask
 from glob import glob
 
 subdir_corr = '/deneb_disk/RodentTools/data/EAE/EAE28_biascor'
@@ -25,15 +25,20 @@ for s in flist:
 
     subfile_corr = os.path.join(subdir_corr, 'm'+subid + '.hdr')
     img_corr = ni.swap_img_hemispheres(subfile_corr)
+    img_corr = ni.new_img_like(img_corr, np.float32(img_corr.get_fdata()))
     img_corr.to_filename(os.path.join(outdir, subid + '_corr.nii.gz'))
 
     subfile = os.path.join(subdir, subid + '.img')
     img_src = nb.load(subfile)
-    img_src = ni.new_img_like(img_corr, img_src.get_fdata())
+    img_src = ni.new_img_like(img_corr, np.float32(img_src.get_fdata()))
     nb.save(img_src, os.path.join(outdir, subid + '_uncorr.nii.gz'))
 
     mask = os.path.join(mask_dir, subid + '.mask.hdr')
     img_msk = nb.load(mask)
-    img_msk = ni.new_img_like(img_corr, np.uint8(img_msk.get_fdata()))
+    img_msk = ni.new_img_like(img_corr, np.uint8(img_msk.get_fdata()).squeeze())
     nb.save(img_msk, os.path.join(outdir, subid + '.mask.nii.gz'))
+
+    # apply mask
+    img_corr = ni.new_img_like(img_corr,np.float32(img_corr.get_fdata() * (img_msk.get_fdata()>0).squeeze()))
+    img_corr.to_filename(os.path.join(outdir, subid + '.bfc.nii.gz'))
 
