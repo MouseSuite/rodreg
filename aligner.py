a6331fe14ab139454b967b9c385fbdd9242e747e
Anand A. Joshi <ajoshi@usc.edu>
2023-11-16 01:15:36 -0800

    updated for new monai version

--- aligner.py
+++ aligner.py
@@ -49,11 +49,11 @@ class Aligner:
             raise AssertionError('Invalid Loss')
 
     def loadMoving(self, moving_file):
-        self.moving, self.moving_meta = LoadImage()(moving_file)
+        self.moving, self.moving_meta = LoadImage(image_only=False)(moving_file)
         self.moving = EnsureChannelFirst()(self.moving)
 
     def loadTarget(self, fixed_file):
-        self.target, self.moving_meta = LoadImage()(fixed_file)
+        self.target, self.moving_meta = LoadImage(image_only=False)(fixed_file)
         self.target = EnsureChannelFirst()(self.target)
 
     def performAffine(self):
